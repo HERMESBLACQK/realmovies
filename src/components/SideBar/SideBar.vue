217f5790105dff49cbe1dc5a0e74d76ca3d07b63
HermesBlacqk <omotosolilhenxy@gmail.com>
2023-06-08 11:26:43 +0100

    ....

--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -4,8 +4,9 @@
     <RouterLink
       :to="{ name: 'HomeView' }"
       class="block py-2 px-3 md:p-4 text-white font-bold uppercase"
-      ><span class="font-semibold">
+      >
       <img class="w-8 h-8" src="../../assets/logo.png" alt="logo" />
+      <span class="font-semibold">
 
         <span class="text-white">Real</span>
         <span class="text-primary">Streams</span>
